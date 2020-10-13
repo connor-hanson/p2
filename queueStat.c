5458c48587cedcc5cafc681ec9a53284da9ded26
reatreify <70864815+reatreify@users.noreply.github.com>
2020-10-12 20:08:40 -0500

    Create queueStat.c

---
+++ queueStat.c
@@ -0,0 +1,63 @@
+//Author: Connor Hanson, Tiger Ji
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
+#include "queue.h"
+#include "queueStat.h"
+
+/**
+ * Prints queue stats
+ *
+ * @param:
+ *	Queue *q - pointer to queue to have stats printed to stderr
+ */
+void printQueueStats(Queue *q){
+	fprintf(stderr,"%s%d\n", "Capacity: ", q->capacity);
+	fprintf(stderr,"%s%d\n", "Enqueue Count: ", q->enqueueCount);
+	fprintf(stderr,"%s%d\n", "Dequeue Count: ", q->dequeueCount);
+	fprintf(stderr,"%s%f\n", "Enqueue Time: ", q->enqueueTime);
+	fprintf(stderr,"%s%f\n", "Dequeue Time: ", q->dequeueTime);
+}
+
+/**
+ * Increments enqueue count
+ *
+ * @param:
+ *	Queue *q - pointer to queue to have stat changed
+ */
+void incEnqueue(Queue *q){
+	q->enqueueCount += 1;
+}
+
+/**
+ * Increments dequeue count
+ *
+ * @param:
+ *	Queue *q - pointer to queue to have stat changed
+ */
+void incDequeue(Queue *q){
+	q->dequeueCount += 1;
+}
+
+
+/**
+ * Adds to total enqueue time
+ *
+ * @param:
+ *	Queue *q - pointer to queue to have stat changed
+ */
+void addETime(Queue *q, double time){
+	q->enqueueTime += time;
+}
+
+/**
+ * Adds to total dequeue time
+ *
+ * @param:
+ *	Queue *q - pointer to queue to have stat changed
+ */
+void addDTime(Queue *q, double time){
+	q->dequeueTime += time;
+}
