3d2b00904f6c1fe1f775d9636bcb5fa28c0f0486
reatreify <70864815+reatreify@users.noreply.github.com>
2020-10-12 20:05:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,122 +3,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
+#include <semaphore.h>
 #include "queue.h"
+#include "pthreadDef.h"
+#include "queueStat.h"
 
-#define BUFF_SIZE 100
 #define QUEUE_SIZE 10
 
 pthread_t read, mun1, mun2, writ;
-
-sem_t resource;
-sem_t writer;
-sem_t reader;
 
 static Queue *q1;
 static Queue *q2;
 static Queue *q3;
 
-// Read from stdio, one line at a time. 
-// Pass each line of the input to Munch1 through a queue of character strings
-void *reader(void *arg){
-	char *input;
-	int nextChar = getc(stdin);
-	while(nextChar != EOF){}
-		if(input = malloc(BUFF_SIZE)){
-			char buffer[BUFF_SIZE] = nextChar;
-			int counter = 1;
-			while(nextChar != EOF && nextChar != '\n'){
-				counter++;
-				nextChar = getc(stdin);
-				if((counter-1) < BUFF_SIZE){
-					buffer[counter-1] = nextChar;
-				}
-			}
-			nextChar = getc(stdin);
-			if(count <= BUFF_SIZE){
-				strcpy(input, buffer);
-				EnqueueString(queue1, input);
-			}
-		}
-	}
-}
-
-// Scan line and replace each space ' ' with '*'
-// Passes this line to munch2 through a queue
-void *munch1(void *arg){
-	char *input = "";
-	while(input != NULL){
-		input = DequeueString(queue1);
-		if(input != NULL){
-			char buffer[BUFF_SIZE];
-			strcpy(buffer, input);
-			int i;
-			for(i = 0; i < BUFF_SIZE; i++){
-				if(buffer[i] == ' '){
-					buffer[i] = '*';
-				}
-			}
-			strcpy(input,buffer);
-		}
-		EnqueueString(queue2, input);
-	}
-}
-
-// Scan line and convert all lowercase to uppercase
-// pass line to writer through a queue
-void *munch2(void *arg){
-	char *input = "";
-	while(input != NULL){
-		input = DequeueString(queue2);
-		if(input != NULL){
-			char buffer[BUFF_SIZE];
-			strcpy(buffer, input);
-			int i;
-			for(i = 0; i < BUFF_SIZE; i++){
-				if(islower(buffer[i]){
-					int c = buffer[i];
-					buffer[i] = toupper(c);
-				}
-			}
-			strcpy(input,buffer);
-		}
-		EnqueueString(queue3, input);
-	}
-}
-
-// write line to stdio
-void *writer(void *arg){
-	char *input = "";
-	while(input != NULL){
-		input = DequeueString(queue3);
-		if(input != NULL){
-			printf("%s\n", input);
-			free(input);
-		}
-	}
-}
-
 int main(){
-	if(!(sem_init(&resource, 0, 1)) && !(sem_init(&writer, 0, 1)) && !(sem_init(&reader, 0, 1))){
-		// make queues
-		Queue *q1 = CreateStringQueue(QUEUE_SIZE);
-		Queue *q2 = CreateStringQueue(QUEUE_SIZE);
-		Queue *q3 = CreateStringQueue(QUEUE_SIZE);
-		// make threads
-		pthread_create(&read, NULL, reader, NULL);
-		pthread_create(&mun1, NULL, munch1, NULL);
-		pthread_create(&mun2, NULL, munch2, NULL);
-		pthread_create(&writ, NULL, writer, NULL);
+	// make queues, exit on failure
+	q1 = createStringQueue(QUEUE_SIZE);
+	q2 = createStringQueue(QUEUE_SIZE);
+	q3 = createStringQueue(QUEUE_SIZE);
+	if(q1 != NULL && q2 != NULL && q3 != NULL){
+		// group queues to pass to thread funcs, exit on failure
+		Queue **readMunch = malloc(2*sizeof(Queue *));
+		Queue **munchMunch = malloc(2*sizeof(Queue *));
+		if(readMunch == NULL || munchMunch == NULL){
+			exit(EXIT_FAILURE);
+		}
+		readMunch[0] = q1;
+		readMunch[1] = q2;
+		munchMunch[0] = q2;
+		munchMunch[1] = q3;
+		// make threads, exit on failure
+		if(pthread_create(&read, NULL, reader, q1)){
+			exit(EXIT_FAILURE);
+		}
+		if(pthread_create(&mun1, NULL, munch1, readMunch)){
+			exit(EXIT_FAILURE);
+		}
+		if(pthread_create(&mun2, NULL, munch2, munchMunch)){
+			exit(EXIT_FAILURE);
+		}
+		if(pthread_create(&writ, NULL, writer, q3)){
+			exit(EXIT_FAILURE);
+		}
 		// join with terminated threads
 		pthread_join(read, NULL);
 		pthread_join(mun1, NULL);
 		pthread_join(mun2, NULL);
 		pthread_join(writ, NULL);
-		// free resources
-		sem_destroy(&resource);
-		sem_destory(&writer);
-		sem_destory(&reader);
+		// print stderr stats
+		fprintf(stderr, "Queue 1 Statistics:\n");
+		printQueueStats(q1);
+		fprintf(stderr, "Queue 2 Statistics:\n");
+		printQueueStats(q2);
+		fprintf(stderr, "Queue 3 Statistics:\n");
+		printQueueStats(q3);
+		// free queue groups
+		free(readMunch);
+		free(munchMunch);
+		// free queue elements
+		freeQueue(q1);
+		freeQueue(q2);
+		freeQueue(q3);
+		// free queues
+		free(q1);
+		free(q2);
+		free(q3);
+	}
+	else{
+		exit(EXIT_FAILURE);
 	}
 	return 0;
 }
